f46a6d1c4bd2640cb781f78f930c14bd6ec0e6c2
Erin Walker <erinb0317@hotmail.com>
2018-06-02 23:56:20 +0000

    automatically backed up by learn

--- db/migrate/20180602234037_create_users.rb
+++ db/migrate/20180602234037_create_users.rb
@@ -2,7 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :uid
+      t.string :email
+      t.string :image 
+      t.string :uid
 
       t.timestamps null: false
     end
