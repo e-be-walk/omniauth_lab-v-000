b9802c9ee89c253ca0f3b2512de0f1a8b40bc727
Erin Walker <erinb0317@hotmail.com>
2018-06-02 23:55:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get '/auth/facebook/callback' => 'sessions#create'
 
   resources :users, :sessions
-  
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602234037) do
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
